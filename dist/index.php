cf00cf7faac364570d32c525090c889959efe39c
Don <don.steele@gmail.com>
2016-09-05 17:16:23 -0400

    Added Minified version of css

---
+++ dist/index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Video's</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+</head>
+<style>
+.panel-overlay,a,label select{cursor:pointer}video{display:block;margin:0 auto;width:70%!important;height:auto!important;position:relative;top:50%;transform:translateY(-50%)}.panel,body,html{height:100%}html{-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background:#000;background:-webkit-linear-gradient(to bottom,#000,#434343);background:linear-gradient(to bottom,#000,#434343)}body{overflow:hidden}.border-box{box-sizing:border-box;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;-o-box-sizing:border-box;-ms-box-sizing:border-box}.panel{transition:.2s;transition:all .2s ease-in-out;-webkit-transition:all .2s ease-in-out;-moz-transition:all .2s ease-in-out;-o-transition:all .2s ease-in-out;-ms-transition:all .2s ease-in-out;position:fixed;z-index:9999;top:0;width:450px}.panel ul{list-style-type:none;position:absolute;z-index:1;margin:0;padding:0;top:20%;left:60px;right:60px;height:auto;text-align:left}.panel ul a,.panel ul li{display:inline-block;float:left;width:100%;line-height:1.4;padding:0;font-weight:200;text-overflow:ellipsis;white-space:nowrap;overflow:hidden}.panel ul li.menu-label{font-size:16px;margin:30px 0 10px}.panel ul li.menu-label:first-child{margin-top:0}.panel ul a{font-size:22px;text-transform:uppercase}.panel ul li.menu-contacts a{font-size:16px;text-transform:inherit}.panel .ninja-btn{transition:.2s;transition:all .2s ease-in-out;-webkit-transition:all .2s ease-in-out;-moz-transition:all .2s ease-in-out;-o-transition:all .2s ease-in-out;-ms-transition:all .2s ease-in-out;display:inline-block;float:left;cursor:pointer;padding:34.29px 30px;position:absolute;z-index:2;top:0;width:30px;height:20.18px}.panel .ninja-btn span,.panel .ninja-btn span:after,.panel .ninja-btn span:before{transition:.3s;transition:all .3s ease-in-out;-webkit-transition:all .3s ease-in-out;-moz-transition:all .3s ease-in-out;-o-transition:all .3s ease-in-out;-ms-transition:all .3s ease-in-out;width:30px;height:2px}.panel .ninja-btn span{position:relative;display:inline-block;float:left;margin-top:9.09px}.panel .ninja-btn span:after,.panel .ninja-btn span:before{content:"";position:absolute;left:0}.panel .ninja-btn span:before{top:-9.09px}.panel .ninja-btn span:after{top:9.09px}.panel .ninja-btn.active span{background-color:transparent}.panel .ninja-btn.active span:after,.panel .ninja-btn.active span:before{top:0}.panel .ninja-btn.active span:before{transform:rotate(-135deg);-webkit-transform:rotate(-135deg);-moz-transform:rotate(-135deg);-o-transform:rotate(-135deg);-ms-transform:rotate(-135deg)}.panel .ninja-btn.active span:after{-webkit-transition-delay:90ms;-moz-transition-delay:90ms;-o-transition-delay:90ms;-msa-transition-delay:90ms;transition-delay:90ms;transform:rotate(135deg);-webkit-transform:rotate(135deg);-moz-transform:rotate(135deg);-o-transform:rotate(135deg);-ms-transform:rotate(135deg)}.panel-overlay{position:fixed;top:0;z-index:9998;width:100%;height:100%;display:none;background-color:rgba(0,0,0,.35);background:-webkit-gradient(linear,left top,left bottom,color-stop(0,rgba(0,0,0,.05)),color-stop(100%,rgba(0,0,0,.6)))}.panel-white,.panel-white .ninja-btn{background-color:#FFF}.panel-white ul a,.panel-white ul li{color:#8e8e93}.panel-white ul li a:hover,.panel-white ul li.active a{color:#007aff}.panel-white .ninja-btn span,.panel-white .ninja-btn span:after,.panel-white .ninja-btn span:before{background-color:#007aff}.panel-black,.panel-black .ninja-btn{background-color:#000}.panel-black ul a,.panel-black ul li{color:#fff}.panel-black ul li a:hover,.panel-black ul li.active a{color:#FFF}.panel-black .ninja-btn span,.panel-black .ninja-btn span:after,.panel-black .ninja-btn span:before,.panel-brand,.panel-brand .ninja-btn{background-color:#007aff}.panel-brand ul a,.panel-brand ul li{color:rgba(0,0,0,.5)}.panel-brand ul li a:hover,.panel-brand ul li.active a{color:#FFF}.panel-brand .ninja-btn span,.panel-brand .ninja-btn span:after,.panel-brand .ninja-btn span:before{background-color:#FFF}.panel.left{left:-450px}.panel.left .ninja-btn{left:100%}.panel.left.active{left:0}.panel.left.active .ninja-btn{left:100%;margin-left:-90px}.panel.right{right:-450px}.panel.right .ninja-btn{right:100%}.panel.right.active{right:0}.panel.right.active .ninja-btn{right:100%;margin-right:-90px}label:after,label:before{right:2px;width:37px;position:absolute;pointer-events:none}label,label select{position:relative;margin:0}label{display:inline-block}label:after{content:'▼';color:#999;font-weight:700;font-size:16px;bottom:8px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;z-index:2;text-align:center}label:before{content:'';top:2px;height:34px;background:#242424;display:block;z-index:1;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px}label select{width:250px;-webkit-appearance:none;-moz-appearance:none;appearance:none;background:#111;color:#999;border:none;outline:0;font-size:14px;padding:10px 9px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;height:38px}
+</style>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <video id=viddiv controls poster="poster.png">
+        <source id="vidsrc" src="" type="video/mp4">
+    </video>
+    <!-- Menu -->
+    <div class="panel panel-black left">
+        <!-- Overlay div -->
+        <div class="overlay"></div>
+        <!-- Animated button -->
+        <a class="ninja-btn" title="menu"><span></span></a>
+        <!-- Menu list -->
+        <ul>
+            <li class="menu-label">Available Video's</li>
+            <li>
+                <label>
+                    <select name='options' class='vidSource'>
+                        <option value=''>Please Select a video</option>
+                        <?php
+                          //fetch and list all the files found in the video folder. Make sure to change the path to your video folder.
+                          
+                          $replacements = array('./video/','.mp4');
+                          
+                          $videos=glob('./video/*.mp4');
+                          
+                          //sort so the newest file is first. 
+                          usort($videos, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
+                          foreach($videos as $video){
+                             $videoname = str_replace($replacements,'',$video);
+                              echo " <option class='isVideo' value='$video'>$videoname</option>";
+                          }
+                        ?>
+                    </select>
+                </label>
+            </li>
+        </ul>
+    </div>
+    <!-- panel -->
+    <div class="panel-overlay"></div>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script>
+    $(window).load(function() {
+
+        $(".ninja-btn, .panel-overlay, .panel li a ").click(function() {
+            toggleMenuState();
+        });
+
+    });
+
+
+    $(window).on("load resize", function() {
+        var menuHeightOffset = $(".panel").find("ul").height() / 2;
+
+        $(".panel").find("ul").css({
+            "margin-top": -menuHeightOffset
+        });
+    });
+
+    function toggleMenuState() {
+
+        $(".ninja-btn, .panel-overlay, .panel").toggleClass("active");
+        /* Check panel overlay */
+        if ($(".panel-overlay").hasClass("active")) {
+            $(".panel-overlay").fadeIn();
+        } else {
+            $(".panel-overlay").fadeOut();
+        }
+    }
+
+    $(document).ready(function() {
+        $('.vidSource').on('change', function() {
+            $('#vidsrc').attr('src', $(this).val());
+            $('#viddiv').attr('poster', 'loading.png');
+            $('#viddiv')[0].load();
+            $('#viddiv')[0].play();
+            toggleMenuState();
+            return false;
+        });
+    });
+    </script>
+</body>
+
+</html>
